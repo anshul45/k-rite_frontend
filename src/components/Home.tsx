d3988e4779aea262916cbf18b8895830b5f1f1bf
anshul45 <anshulgarwal025@gmail.com>
2024-03-18 11:17:39 +0530

    added search functionality

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,15 @@
 
+import { useState } from 'react'
 import Filters from './Filters'
 import Header from './Header'
 
 const Home = () => {
+    const [search, setSearch] = useState("")
+    console.log(search)
     return (
-        <div className='flex-[1.9] w-full mx-2 my-2   py-2.5 border-[1px] rounded-md'>
-            <Header />
-            <Filters />
+        <div className='flex-[1.9] w-full mx-2 my-2   py-1 border-[1px] rounded-md'>
+            <Header search={search} setSearch={setSearch} />
+            <Filters search={search} />
         </div>
     )
 }
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,16 +1,10 @@
 
-const Table = () => {
-    const dummyData = [
-        { brand: 'Brand 1', description: 'Description 1', members: 5, categories: ['Cat A', 'Cat B'], tags: ['Tag 1', 'Tag 2'], nextMeeting: '2024-03-18' },
-        { brand: 'Brand 2', description: 'Description 2', members: 8, categories: ['Cat C', 'Cat D'], tags: ['Tag 3', 'Tag 4'], nextMeeting: '2024-03-20' },
-        { brand: 'Brand 3', description: 'Description 3', members: 3, categories: ['Cat E', 'Cat F'], tags: ['Tag 5', 'Tag 6'], nextMeeting: '2024-03-22' },
-        { brand: 'Brand 3', description: 'Description 3', members: 3, categories: ['Cat E', 'Cat F'], tags: ['Tag 5', 'Tag 6'], nextMeeting: '2024-03-22' },
+const Table = ({ dummyData }: any) => {
 
-    ];
 
     return (
         <>
-            <div className='grid grid-flow-col grid-cols-6  divide-x-[1px] border-b-[1px] border-t-[1px]'>
+            <div className='grid grid-flow-col grid-cols-6  divide-x-[1px] border-b-[1px] border-t-[1px]  text-gray-500'>
                 <div className='col-span-1 px-2 py-1.5'><input type="checkbox" className="accent-black rounded-md mr-1.5" />Brand</div>
                 <div className='col-span-1 px-2 py-1.5'>Description</div>
                 <div className='col-span-1 px-2 py-1.5'>Members</div>
@@ -19,24 +13,24 @@ const Table = () => {
                 <div className='col-span-1 px-2 py-1.5'>Next meeting</div>
                 <div className='px-2'><i className="ri-add-line"></i></div>
             </div>
-            {dummyData.map((data, index) => (
+            {dummyData.map((data: any, index: any) => (
                 <div className='grid grid-flow-col grid-cols-6 divide-x-[1px] border-b-[1px]' key={index}>
-                    <div className='col-span-1 px-2 py-1.5'><input type="checkbox" className="accent-black rounded-md mr-1.5" />{data.brand}</div>
-                    <div className='col-span-1 px-2 py-1.5'>{data.description}</div>
-                    <div className='col-span-1 px-2 py-1.5'>{data.members}</div>
-                    <div className='col-span-1 px-2 py-1.5'>{data.categories}</div>
-                    <div className='col-span-1 px-2 py-1.5'>{data.tags}</div>
-                    <div className='col-span-1 px-2 py-1.5'>{data.nextMeeting}</div>
+                    <div className='col-span-1 px-2 py-1 flex items-center gap-2 text-sm font-bold'><input type="checkbox" className="accent-black rounded-md mr-1.5" /><img className="w-7 h5" alt="image" src={data.url} />{data.brand}</div>
+                    <div className='col-span-1 px-2 py-1 text-sm font-bold text-nowrap overflow-x-hidden'>{data.description.length > 20 ? `${(data.description.slice(0, 20))}...` : data.description}</div>
+                    <div className='col-span-1 px-2 py-1'>{data.members}</div>
+                    <div className='col-span-1 px-2 py-1 text-sm'>{data.categories}</div>
+                    <div className='col-span-1 px-2 py-1 text-sm'> <span className="border-[1px] bg-gray-100 px-1 py-0.5 rounded-md">{data.tags}</span></div>
+                    <div className='col-span-1 px-2 py-1 text-sm'>{data.nextMeeting}</div>
                     <div className='px-2'><i className="ri-add-line text-base text-white"></i></div>
                 </div >
             ))}
             <div className='grid grid-flow-col grid-cols-6  divide-x-[1px] border-b-[1px] text-right font-semibold text-sm text-gray-500'>
-                <div className='col-span-1 px-2 py-1.5'><span className="mr-1">{dummyData.length}</span>count</div>
-                <div className='col-span-1 px-2 py-1.5'><i className="ri-add-line mr-1"></i> Add calculation</div>
-                <div className='col-span-1 px-2 py-1.5'><i className="ri-add-line mr-1"></i> Add calculation</div>
-                <div className='col-span-1 px-2 py-1.5'><i className="ri-add-line mr-1"></i> Add calculation</div>
-                <div className='col-span-1 px-2 py-1.5'><i className="ri-add-line mr-1"></i> Add calculation</div>
-                <div className='col-span-1 px-2 py-1.5'></div>
+                <div className='col-span-1 px-2 py-1'><span className="mr-1">{dummyData.length}</span>count</div>
+                <div className='col-span-1 px-2 py-1'><i className="ri-add-line mr-1"></i> Add calculation</div>
+                <div className='col-span-1 px-2 py-1'><i className="ri-add-line mr-1"></i> Add calculation</div>
+                <div className='col-span-1 px-2 py-1'><i className="ri-add-line mr-1"></i> Add calculation</div>
+                <div className='col-span-1 px-2 py-1'><i className="ri-add-line mr-1"></i> Add calculation</div>
+                <div className='col-span-1 px-2 py-1'></div>
                 <div className='px-2'><i className="ri-add-line text-base text-white"></i></div>
             </div>
         </>
